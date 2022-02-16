919265a83cf1fb9f3a1c2577623e67ce368e83f8
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-02-16 08:07:09 -0300

    List Style and Navigation View

---
+++ SwiftUI for iOS 15/AccountView.swift
@@ -0,0 +1,32 @@
+//
+//  AccountView.swift
+//  SwiftUI for iOS 15
+//
+//  Created by João Carlos Magalhães on 16/02/22.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    Text("Settings")
+                    Text("Billing")
+                    Text("Help")
+                }
+                .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
