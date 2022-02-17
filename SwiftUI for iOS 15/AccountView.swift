bc0944a38f3a38e0193536b7f8867fcb542eb6e0
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-02-17 04:35:19 -0300

    Navigation Link and URL

--- SwiftUI for iOS 15/AccountView.swift
+++ SwiftUI for iOS 15/AccountView.swift
@@ -38,13 +38,41 @@ struct AccountView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding()
+                
                 Section {
-                    Label("Settings", systemImage: "gear")
-                    Label("Billing", systemImage: "creditcard")
-                    Label("Help", systemImage: "questionmark")
-                        .imageScale(.small)
+                    NavigationLink(destination: ContentView()){
+                        Label("Settings", systemImage: "gear")
+                    }
+                    NavigationLink { Text("Billing") } label: {
+                        Label("Billing", systemImage: "creditcard")
+                    }
+                    NavigationLink { ContentView() } label: {
+                        Label("Help", systemImage: "questionmark")
+                    }
                 }
+                .accentColor(.primary)
                 .listRowSeparatorTint(.blue)
+                .listRowSeparator(.hidden)
+                
+                Section{
+                    Link(destination: URL(string: "https://apple.com")!){
+                        HStack{
+                            Label("Website", systemImage: "house")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    Link(destination: URL(string: "https://youtube.com")!){
+                        HStack{
+                            Label("YouTube", systemImage: "tv")
+                            Spacer()
+                            Image(systemName: "link")
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                }
+                .accentColor(.primary)
                 .listRowSeparator(.hidden)
             }
             .listStyle(.insetGrouped)
