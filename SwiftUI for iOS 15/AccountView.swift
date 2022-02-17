ce4fe57293dc4ed77e71af149a8d4b1f0dad6725
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-02-17 04:17:38 -0300

    SF Symbols 3

--- SwiftUI for iOS 15/AccountView.swift
+++ SwiftUI for iOS 15/AccountView.swift
@@ -11,10 +11,38 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack(spacing: 8){
+                    Image(systemName:
+                            "person.crop.circle.fill.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                        )
+                    Text("João Carlos Magalhães")
+                        .font(.title.weight(.semibold))
+                    HStack{
+                        Image(systemName: "location")
+                            .imageScale(.large)
+                        Text("Canada")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
                 Section {
-                    Text("Settings")
-                    Text("Billing")
-                    Text("Help")
+                    Label("Settings", systemImage: "gear")
+                    Label("Billing", systemImage: "creditcard")
+                    Label("Help", systemImage: "questionmark")
+                        .imageScale(.small)
                 }
                 .listRowSeparatorTint(.blue)
                 .listRowSeparator(.hidden)
