f4e47b9e18794a03a245d68cf7e275e5d64f3cd9
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-03-29 16:15:59 -0300

    SVG to Path in Canvas

---
+++ SwiftUI for iOS 15/BlobView.swift
@@ -0,0 +1,51 @@
+//
+//  BlobView.swift
+//  SwiftUI for iOS 15
+//
+//  Created by João Carlos Magalhães on 29/03/22.
+//
+
+import SwiftUI
+
+struct BlobView: View {
+    var body: some View {
+        Canvas { context, size in
+            context.fill(path(in: CGRect(x: 0, y: 0, width: size.width, height: size.height)), with: .linearGradient(Gradient(colors: [.pink, .blue]), startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 400, y: 400)))
+        }
+        .frame(width: 400, height: 414)
+
+    }
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.9923*width, y: 0.42593*height))
+        path.addCurve(to: CGPoint(x: 0.6355*width, y: height), control1: CGPoint(x: 0.91864*width, y: 0.77749*height), control2: CGPoint(x: 0.91864*width, y: height))
+        path.addCurve(to: CGPoint(x: 0.08995*width, y: 0.60171*height), control1: CGPoint(x: 0.35237*width, y: height), control2: CGPoint(x: 0.2695*width, y: 0.77304*height))
+        path.addCurve(to: CGPoint(x: 0.34086*width, y: 0.06324*height), control1: CGPoint(x: -0.0896*width, y: 0.43038*height), control2: CGPoint(x: 0.002484*width, y: 0.23012*height))
+        path.addCurve(to: CGPoint(x: 0.9923*width, y: 0.42593*height), control1: CGPoint(x: 0.67924*width, y: -0.10364*height), control2: CGPoint(x: 1.05906*width, y: 0.07436*height))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct BlobView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlobView()
+    }
+}
+
+struct BlobShape: Shape{
+    func path(in rect: CGRect) -> Path{
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0.9923*width, y: 0.42593*height))
+        path.addCurve(to: CGPoint(x: 0.6355*width, y: height), control1: CGPoint(x: 0.91864*width, y: 0.77749*height), control2: CGPoint(x: 0.91864*width, y: height))
+        path.addCurve(to: CGPoint(x: 0.08995*width, y: 0.60171*height), control1: CGPoint(x: 0.35237*width, y: height), control2: CGPoint(x: 0.2695*width, y: 0.77304*height))
+        path.addCurve(to: CGPoint(x: 0.34086*width, y: 0.06324*height), control1: CGPoint(x: -0.0896*width, y: 0.43038*height), control2: CGPoint(x: 0.002484*width, y: 0.23012*height))
+        path.addCurve(to: CGPoint(x: 0.9923*width, y: 0.42593*height), control1: CGPoint(x: 0.67924*width, y: -0.10364*height), control2: CGPoint(x: 1.05906*width, y: 0.07436*height))
+        path.closeSubpath()
+        return path
+    }
+}
