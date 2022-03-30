7fe0828001252bf7518fca8cc78fe11577252092
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-03-30 14:51:54 -0300

    Tab Bar Selection

--- SwiftUI for iOS 15/Tab.swift
+++ SwiftUI for iOS 15/Tab.swift
@@ -11,11 +11,19 @@ struct TabItem: Identifiable{
     var id = UUID()
     var text: String
     var icon: String
+    var tab: Tab
 }
 
 var tabItems = [
-    TabItem(text: "Learn Now", icon: "house"),
-    TabItem(text: "Explore", icon: "magnifyingglass"),
-    TabItem(text: "Notifications", icon: "bell"),
-    TabItem(text: "Library", icon: "rectangle.stack"),
+    TabItem(text: "Learn Now", icon: "house", tab: .home),
+    TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore),
+    TabItem(text: "Notifications", icon: "bell", tab: .notifications),
+    TabItem(text: "Library", icon: "rectangle.stack", tab: .library),
 ]
+
+enum Tab: String{
+    case home
+    case explore
+    case notifications
+    case library
+}
--- SwiftUI for iOS 15/TabBar.swift
+++ SwiftUI for iOS 15/TabBar.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TabBar: View {
+    @State var selectedTab: Tab = .home
     var body: some View {
         //Default TabView
 //        TabView {
@@ -23,27 +24,45 @@ struct TabBar: View {
 //                }
 //        }
         ZStack(alignment: .bottom){
-            ContentView()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            Group{
+                switch selectedTab {
+                case .home:
+                    ContentView()
+                case .explore:
+                    AccountView()
+                case .notifications:
+                    AccountView()
+                case .library:
+                    AccountView()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
             HStack{
                 ForEach(tabItems){ item in
-                    VStack(spacing: 0){
-                        Image(systemName: item.icon)
-                            .symbolVariant(.fill)
-                            .font(.body.bold())
-                            .frame(width: 80, height: 29)
-                        Text(item.text)
-                            .font(.caption2)
-                            .lineLimit(1)
+                    Button{
+                        selectedTab = item.tab
+                    } label: {
+                        VStack(spacing: 0){
+                            Image(systemName: item.icon)
+                                .symbolVariant(.fill)
+                                .font(.body.bold())
+                                .frame(width: 80, height: 29)
+                            Text(item.text)
+                                .font(.caption2)
+                                .lineLimit(1)
+                        }
+                        .frame(maxWidth: .infinity)
                     }
-                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
                 }
+                
             }
             .padding(.horizontal, 8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
-            .strokeStyle()
+            .strokeStyle(cornerRadious: 34)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
         }
