c41e985766788a41a9d1f004fba89cbb549095b2
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-03-31 09:09:20 -0300

    Detect Scroll with Coordinate Space

---
+++ SwiftUI for iOS 15/Model/PreferenceKeys.swift
@@ -0,0 +1,16 @@
+//
+//  PreferenceKeys.swift
+//  SwiftUI for iOS 15
+//
+//  Created by João Carlos Magalhães on 31/03/22.
+//
+
+import SwiftUI
+
+
+struct ScrollPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
--- SwiftUI for iOS 15/Views/HomeView.swift
+++ SwiftUI for iOS 15/Views/HomeView.swift
@@ -8,18 +8,39 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var hasScrolled = false
     var body: some View {
         ScrollView {
+            
+            GeometryReader{ proxy in
+//                Text("\(proxy.frame(in: .named("scroll")).minY)")
+                Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+            }
+            .frame(height: 0)
+            
             FeaturedItem()
             
             Color.clear.frame(height: 1000)
         }
+        .coordinateSpace(name: "scroll")
+        
+        .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+            withAnimation(.easeInOut){
+                if value < 0 {
+                    hasScrolled = true
+                } else {
+                    hasScrolled = false
+                }
+            }
+            
+        })
         //scrollbar
         .safeAreaInset(edge: .top, content: {
             Color.clear.frame(height: 70)
         })
         .overlay(
             NavigationBar(title: "Featured")
+                .opacity(hasScrolled ? 1 : 0)
         )
             
     }
