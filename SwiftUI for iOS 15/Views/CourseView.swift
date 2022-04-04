bab41cc94753d4891ae35dbc77fd063fa272031f
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-04-04 10:57:52 -0300

    Scroll Parallax

--- SwiftUI for iOS 15/Views/CourseView.swift
+++ SwiftUI for iOS 15/Views/CourseView.swift
@@ -39,66 +39,43 @@ struct CourseView: View {
     }
     
     var cover: some View {
+        GeometryReader { proxy in
+            let scrollY = proxy.frame(in: .global).minY
+            
             VStack {
-                Spacer()
-            }
-            .frame(maxWidth: .infinity)
-            .frame(height: 500)
-            .foregroundStyle(.black)
-            .background(
-                Image(course.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .matchedGeometryEffect(id: "image\(course.id)", in: namespace)
-                
-            )
-            .background(
-                Image(course.background)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .matchedGeometryEffect(id: "background\(course.id)", in: namespace)
-            )
-            .mask(
-                RoundedRectangle(cornerRadius: 30, style: .continuous)
-                    .matchedGeometryEffect(id: "mask\(course.id)", in: namespace)
-            )
-            .overlay(
-                VStack(alignment: .leading, spacing: 12){
-                    Text(course.title)
-                        .font(.largeTitle.weight(.bold))
-                        .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text(course.subtitle.uppercased())
-                        .font(.footnote.weight(.semibold))
-                        .matchedGeometryEffect(id: "subtitle\(course.id)", in: namespace)
-                    Text(course.text)
-                        .font(.footnote)
-                        .matchedGeometryEffect(id: "text\(course.id)", in: namespace)
-                    Divider()
-                        .opacity(appear[0] ? 1 : 0)
-                    HStack{
-                        Image("Avatar Default")
-                            .resizable()
-                            .frame(width: 26, height: 26)
-                            .cornerRadius(10)
-                            .padding(8)
-                            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
-                            .strokeStyle(cornerRadious: 18)
-                        Text("Taught by John Carlos").font(.footnote)
-                    }
-                    .opacity(appear[1] ? 1 : 0)
+                    Spacer()
                 }
-                    .padding(20)
-                    .background(
-                        Rectangle()
-                            .fill(.ultraThinMaterial)
-                            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                            .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
+                .frame(maxWidth: .infinity)
+                .frame(height: scrollY > 0 ? 500 + scrollY : 500)
+                .foregroundStyle(.black)
+                .background(
+                    Image(course.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: "image\(course.id)", in: namespace)
+                        .offset(y: scrollY > 0 ? scrollY * -0.8 : 0)
                     
-                    )
-                    .offset(y: 250)
-                    .padding(20)
-            )
+                )
+                .background(
+                    Image(course.background)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background\(course.id)", in: namespace)
+                        .offset(y: scrollY > 0 ? -scrollY : 0)
+                        .scaleEffect(scrollY > 0 ? scrollY / 1000 + 1 : 1)
+                        .blur(radius: scrollY / 10)
+                )
+                .mask(
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask\(course.id)", in: namespace)
+                        .offset(y: scrollY > 0 ? -scrollY : 0)
+                )
+                .overlay(
+                    overlayContent
+                        .offset(y: scrollY > 0 ? scrollY * -0.6 : 0)
+                )
+        }
+        .frame(height: 500)
     }
     
     var content: some View {
@@ -132,6 +109,46 @@ struct CourseView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
         .padding(20)
         .ignoresSafeArea()
+    }
+    
+    var overlayContent: some View {
+        
+            VStack(alignment: .leading, spacing: 12){
+                Text(course.title)
+                    .font(.largeTitle.weight(.bold))
+                    .matchedGeometryEffect(id: "title\(course.id)", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(course.subtitle.uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .matchedGeometryEffect(id: "subtitle\(course.id)", in: namespace)
+                Text(course.text)
+                    .font(.footnote)
+                    .matchedGeometryEffect(id: "text\(course.id)", in: namespace)
+                Divider()
+                    .opacity(appear[0] ? 1 : 0)
+                HStack{
+                    Image("Avatar Default")
+                        .resizable()
+                        .frame(width: 26, height: 26)
+                        .cornerRadius(10)
+                        .padding(8)
+                        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                        .strokeStyle(cornerRadious: 18)
+                    Text("Taught by John Carlos").font(.footnote)
+                }
+                .opacity(appear[1] ? 1 : 0)
+            }
+                .padding(20)
+                .background(
+                    Rectangle()
+                        .fill(.ultraThinMaterial)
+                        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+                        .matchedGeometryEffect(id: "blur\(course.id)", in: namespace)
+                
+                )
+                .offset(y: 250)
+                .padding(20)
+        
     }
     
     func fadeIn() {
