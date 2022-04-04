c744979ad3cae170fd3b82cac78ac69abbe7c969
João Carlos Magalhães <joaocarlosminformatica@gmail.com>
2022-04-04 11:38:15 -0300

    Edge Pan Gesture

--- SwiftUI for iOS 15/Views/CourseView.swift
+++ SwiftUI for iOS 15/Views/CourseView.swift
@@ -14,11 +14,11 @@ struct CourseView: View {
     @State var appear = [false, false, false]
     @EnvironmentObject var model: Model
     @State var viewState: CGSize = .zero
+    @State var isDraggable = true
     
     var body: some View {
         ZStack {
             ScrollView{
-                
                 cover
                 
                 content
@@ -33,16 +33,7 @@ struct CourseView: View {
             .background(.black.opacity(viewState.width / 500))
             .background(.ultraThinMaterial)
             .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        guard value.translation.width > 0 else { return }
-                        viewState = value.translation
-                    }
-                    .onEnded { value in
-                        withAnimation(.closeCard){
-                            viewState = .zero
-                        }
-                    }
+                isDraggable ? drag : nil
             )
             .ignoresSafeArea()
             
@@ -169,6 +160,34 @@ struct CourseView: View {
         
     }
     
+    var drag : some Gesture {
+        DragGesture(minimumDistance: 30, coordinateSpace: .local)
+            .onChanged { value in
+                guard value.translation.width > 0 else { return }
+                
+                if value.startLocation.x < 100 {
+                    withAnimation(.closeCard){
+                        viewState = value.translation
+                    }
+                }
+                
+                if viewState.width > 120 {
+                    close()
+                }
+            }
+            .onEnded { value in
+                if viewState.width > 80 {
+                    close()
+                } else {
+                    withAnimation(.closeCard){
+                        viewState = .zero
+                    }
+                }
+                
+                
+            }
+    }
+    
     func fadeIn() {
         withAnimation(.easeOut.delay(0.3)) {
             appear[0] = true
@@ -185,6 +204,19 @@ struct CourseView: View {
         appear[0] = false
         appear[1] = false
         appear[2] = false
+    }
+    
+    func close(){
+        withAnimation(.closeCard.delay(0.3)){
+            show.toggle()
+            model.showDetail.toggle()
+        }
+        
+        withAnimation(.closeCard){
+            viewState = .zero
+        }
+        
+        isDraggable = false
     }
 }
 
